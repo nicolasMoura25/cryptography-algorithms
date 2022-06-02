6bad82c82949046a8e841b2b0463102dfadd1a95
Rafael Garibotti <67865142+rafaelgaribotti@users.noreply.github.com>
2022-06-02 08:21:16 -0300

    Removed Decrypt

--- algorithms/IDEA/IDEA.h
+++ algorithms/IDEA/IDEA.h
@@ -15,11 +15,9 @@
 typedef struct
 {
 	uint16_t encryptionKeys[52];
-	uint16_t decryptionKeys[52];
 } IdeaContext;
 
 void IDEA_init(IdeaContext* context, uint16_t* key);
 void IDEA_encrypt(IdeaContext* context, uint16_t* block, uint16_t* out);
-void IDEA_decrypt(IdeaContext* context, uint16_t* encryptedBlock, uint16_t* out);
 
 void IDEA_main(CTRCounter* ctrNonce, int key_size);
