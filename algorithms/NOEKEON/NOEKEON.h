9268f412c69f86fdd9fcd4428a4b127010d6ed4d
Rafael Garibotti <67865142+rafaelgaribotti@users.noreply.github.com>
2022-06-02 08:22:56 -0300

    Removed Decrypt

--- algorithms/NOEKEON/NOEKEON.h
+++ algorithms/NOEKEON/NOEKEON.h
@@ -12,6 +12,5 @@
 #include "../../CTRMode.h"
 
 void NOEKEON_encrypt(uint32_t* block, uint32_t* key, uint32_t* encryptdBlock);
-void NOEKEON_decrypt(uint32_t* encryptedBlock, uint32_t* key, uint32_t* decryptedBlock);
 
 void NOEKEON_main(CTRCounter* ctrNonce, int key_size);
