23e08f5e166d20d369c364752a75db7c789f53d7
Rafael Garibotti <67865142+rafaelgaribotti@users.noreply.github.com>
2022-06-01 21:51:21 -0300

    Removed Decrypt

--- algorithms/ARIA/ARIA.h
+++ algorithms/ARIA/ARIA.h
@@ -16,11 +16,9 @@ typedef struct
 	uint32_t rounds;
 	// each subkey is 4 parts of 32 bits
 	uint32_t eks[17][4];
-	uint32_t dks[17][4];
 } AriaContext;
 
 void ARIA_init(AriaContext* context, const uint32_t* key, uint32_t keyLength);
 void ARIA_encrypt(AriaContext* context, uint32_t* block, uint32_t* P);
-void ARIA_decrypt(AriaContext* context, uint32_t* block, uint32_t* P);
 
 void ARIA_main(CTRCounter* ctrCounter, int key_size);
